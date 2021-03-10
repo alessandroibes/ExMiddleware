4e970e52fe15d483157b47dfb6ec5410a3cf0bd7
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-10 20:57:58 -0300

    Código completo.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExMiddleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
